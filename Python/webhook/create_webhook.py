03278619d556d3041938f17291183ff930d55c70
mndprdy <mani.deepu99@gmail.com>
2024-06-06 04:40:31 +0530

    webhook integration changes

--- Python/webhook/create_webhook.py
+++ Python/webhook/create_webhook.py
@@ -1,32 +1,3 @@
-# from flask import Flask, request
-# import requests
-
-# app = Flask(__name__)
-# subscription_activated = False
-
-# @app.route('/webhook', methods=['POST'])
-# def webhook():
-#     global subscription_activated
-#     data = request.json
-
-#     if 'subscription_url' in data and not subscription_activated:
-#         subscription_url = data['subscription_url']
-#         response = requests.get(subscription_url)
-#         if response.status_code == 200:
-#             subscription_activated = True
-#             return 'Subscription activated successfully', 200
-#         else:
-#             return 'Failed to activate subscription', 400
-
-#     else:
-#         print(data)
-#         return 'Request received successfully', 200
-
-# if __name__ == '__main__':
-#     app.run(host= "10.10.6.246" ,port=5000 ,debug=True)
-
-
-
 from flask import Flask, request
 import requests
 
